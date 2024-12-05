9a23b80792f5cada3a6822227d43dfca34497995
Your Name <you@example.com>
2024-12-05 15:18:08 +0700

    hide log

--- src/movie_booking_system/src/com/mycompany/database/ConnectionManager.java
+++ src/movie_booking_system/src/com/mycompany/database/ConnectionManager.java
@@ -4,9 +4,18 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ConnectionManager {
     private static final String URI = "mongodb+srv://dinhhung1508:hung15082004@cluster0.gnfqipw.mongodb.net/";
     private static final String DATABASE_NAME = "OOP_JAVA"; 
+
+    static {
+
+        Logger mongoLogger = Logger.getLogger("org.mongodb.driver");
+        mongoLogger.setLevel(Level.SEVERE);
+    }
 
     public static MongoDatabase getDatabase() {
         MongoClient client = MongoClients.create(URI);
